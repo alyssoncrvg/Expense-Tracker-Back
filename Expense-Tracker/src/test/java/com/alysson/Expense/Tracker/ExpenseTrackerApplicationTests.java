3241a6bd6074edd001a3b203cc6c7a9ee93c255b
alyssoncrvg <alyssonpereira41@gmail.com>
2025-12-06 16:29:53 -0300

    first commit

---
+++ Expense-Tracker/src/test/java/com/alysson/Expense/Tracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alysson.Expense.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
